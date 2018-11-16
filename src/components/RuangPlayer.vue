e699002872a3f7c732b6d25f3158b8e6877f2a59
zenfinish <zen.finish@gmail.com>
2018-11-16 16:05:48 +0700

    upload

---
+++ src/components/RuangPlayer.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="about">
+        <div>
+            <img src="@/assets/jempolTrue.gif" style="width: 200px; height:auto;" v-if="jempolTrue" />
+            <img src="@/assets/jempol1.gif" style="width: 200px; height:auto;" v-if="jempol1" />
+            <img src="@/assets/jempol2.gif" style="width: 200px; height:auto;" v-if="jempol2" />
+            <img src="@/assets/jempol0.gif" style="width: 200px; height:auto;" v-if="jempol0" />
+        </div>
+        <div class="footer">
+            <button @click="jempol1 = false; jempol2 = false; jempol0 = true; jempolTrue = false;">0</button>
+            <button @click="jempol1 = true; jempol2 = false; jempol0 = false; jempolTrue = false;">1</button>
+            <button @click="jempol1 = false; jempol2 = true; jempol0 = false; jempolTrue = false;">2</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RuangPlayer',
+    data: function() {
+        return {
+            jempolTrue: false,
+            jempol1: false,
+            jempol2: false,
+            jempol0: true,
+        };
+    },
+}
+</script>
+
+<style scoped>
+    .about{
+        position: relative;
+        border: 7px solid; width: 250px; height: 250px;
+    }
+    .footer{
+        position: absolute;
+        bottom: 5px;
+        text-align: center;
+        width: 100%;
+    }
+</style>
+
---
+++ src/views/Terserah.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="about">
+        <h1>Ruang Player</h1>
+        <div class="container">
+            <div class="row" align="center">
+                <div class="col-sm col-6"><RuangPlayer /></div>
+                <div class="col-sm col-6"><RuangPlayer /></div>
+            </div>
+            <div class="row" align="center">
+                <div class="col-sm-12"><RuangPlayer /></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import RuangPlayer from '@/components/RuangPlayer.vue'
+
+export default {
+    name: 'About',
+    components: {
+        RuangPlayer,
+    },
+}
+</script>
